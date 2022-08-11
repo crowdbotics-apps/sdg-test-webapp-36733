51fad0ad907256a62438472b7ba273eaa65be3a6
Crowdbotics <team@crowdbotics.com>
2022-08-11 20:13:19 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Resource, Task
+
+admin.site.register(Task)
+admin.site.register(Resource)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,30 @@
+from django.conf import settings
 from django.db import models
+
+
+class Task(models.Model):
+    "Generated Model"
+    title = models.CharField(
+        max_length=256,
+    )
+    description = models.TextField()
+    hours = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    resource = models.ForeignKey(
+        "home.Resource",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="task_resource",
+    )
+
+
+class Resource(models.Model):
+    "Generated Model"
+    title = models.CharField(
+        max_length=256,
+    )
+    description = models.TextField()
+    rate = models.FloatField()
